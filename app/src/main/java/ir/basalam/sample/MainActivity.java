cbea3355ddefa145fc7e5b0eae20c927096092a8
basalam <m.nasiri13@gmail.com>
2017-08-05 18:47:47 +0430

    Initial commit

---
+++ app/src/main/java/ir/basalam/sample/MainActivity.java
@@ -0,0 +1,97 @@
+package ir.basalam.sample;
+
+import android.os.Bundle;
+import android.support.design.internal.NavigationMenuView;
+import android.support.design.internal.NavigationSubMenu;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import ir.basalam.rtlnavigationview.RtlNavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawer.isDrawerOpen(Gravity.RIGHT)) {
+                    drawer.closeDrawer(Gravity.RIGHT);
+                } else {
+                    drawer.openDrawer(Gravity.RIGHT);
+                }
+            }
+        });
+
+        RtlNavigationView rtlNavigationView = (RtlNavigationView) findViewById(R.id.nav_view);
+        rtlNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(Gravity.RIGHT)) {
+            drawer.closeDrawer(Gravity.RIGHT);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Toast.makeText(this, "CAMERA", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(this, "GALLERY", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_slideshow) {
+            Toast.makeText(this, "SLIDESHOW", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_manage) {
+            Toast.makeText(this, "MANAGE", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(this, "SHARE", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_send) {
+            Toast.makeText(this, "SEND", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(Gravity.RIGHT);
+        return true;
+    }
+}
---
+++ rtl-navigationview/src/main/java/ir/basalam/rtlnavigationview/RtlNavigationView.java
@@ -0,0 +1,84 @@
+package ir.basalam.rtlnavigationview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
+import android.support.v7.view.SupportMenuInflater;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class RtlNavigationView extends NavigationView {
+    private OnNavigationItemSelectedListener navigationItemSelectedListener;
+
+    public RtlNavigationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray array = context.obtainStyledAttributes(attrs, new int[]{R.attr.rtl_menu});
+        // Check if rtl_menu attribute has value
+        if (array.getResourceId(0, -1) != -1) {
+            Menu menu = new NavigationMenu(context);
+            new SupportMenuInflater(context).inflate(array.getResourceId(0, -1), menu);
+            createRtlMenu(menu);
+            array.recycle();
+        }
+    }
+
+    @Override
+    public void setNavigationItemSelectedListener(
+            @Nullable OnNavigationItemSelectedListener listener) {
+        navigationItemSelectedListener = listener;
+    }
+
+    private void createRtlMenu(final Menu rtlMenu) {
+        for (int i = 0; i < rtlMenu.size(); i++) {
+            // Add an empty MenuItem to the NavigationView for each of the items in the rtlMenu
+            final MenuItem addedMenuItem = getMenu().add(null);
+            final MenuItem currentRtlMenuItem = rtlMenu.getItem(i);
+            
+            boolean isGroup = currentRtlMenuItem.hasSubMenu();
+            if (isGroup) {
+                // Set group title layout if current rtlMenu item has a submenu
+                addedMenuItem.setActionView(R.layout.list_item_rtl_navigationview_group_title);
+                // We don't want group title to respond to user clicks
+                addedMenuItem.setEnabled(false);
+
+                TextView tvGroupTitle = addedMenuItem.getActionView()
+                        .findViewById(R.id.list_item_navigationview_group_title_textview);
+                tvGroupTitle.setText(currentRtlMenuItem.getTitle());
+
+                createRtlMenu(currentRtlMenuItem.getSubMenu());
+            } else {
+                // Set normal menu item layout 
+                addedMenuItem.setActionView(R.layout.list_item_rtl_navigationview);
+                addedMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if (navigationItemSelectedListener != null) {
+                            navigationItemSelectedListener
+                                    .onNavigationItemSelected(currentRtlMenuItem);
+                        }
+                        return false;
+                    }
+                });
+
+                TextView tvTitle = addedMenuItem.getActionView()
+                        .findViewById(R.id.list_item_rtl_navigationview_title_textview);
+                ImageView ivIcon = addedMenuItem.getActionView()
+                        .findViewById(R.id.list_item_rtl_navigationview_icon_imageview);
+
+                tvTitle.setText(currentRtlMenuItem.getTitle());
+                Drawable itemIcon = currentRtlMenuItem.getIcon();
+                itemIcon.setColorFilter(getResources().getColor(R.color.colorListItemIcon),
+                        PorterDuff.Mode.SRC_IN);
+                ivIcon.setImageDrawable(itemIcon);
+            }
+        }
+    }
+}
